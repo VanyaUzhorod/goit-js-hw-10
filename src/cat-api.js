d2e8ac4a8860e2f2fc53534a5776d969be679344
VanyaUzhorod <131401354+VanyaUzhorod@users.noreply.github.com>
2023-10-03 22:33:55 +0300

    required

---
+++ src/cat-api.js
@@ -0,0 +1,24 @@
+const url = 'https://api.thecatapi.com/v1';
+const api_key = "live_D4SbxmSHV5tExgiTjp98hnZdSPx3NZSt9FlXsl3NopfepehiiVbBRgdl154nSZsA";
+
+function fetchBreeds() {
+    return fetch(`${url}/breeds?api_key=${api_key}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });       
+};
+
+function fetchCatByBreed(breedId) {
+    return fetch(`${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });  
+};
+
+  export { fetchBreeds, fetchCatByBreed };
